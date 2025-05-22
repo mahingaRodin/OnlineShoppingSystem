71ce81ebac24f233a3ff9fe9c548e9c56c6fbed6
P-Odiix <mahingarodin@gmail.com>
2025-05-22 08:12:34 +0200

    [changes]: finished setting up the security classes

---
+++ src/main/java/com/onlineShoppingBe/beForOnlineShoppingSystem/repositories/IUserRepository.java
@@ -0,0 +1,4 @@
+package com.onlineShoppingBe.beForOnlineShoppingSystem.repositories;
+
+public interface IUserRepository {
+}
---
+++ src/main/java/com/onlineShoppingBe/beForOnlineShoppingSystem/util/UserServiceImpl.java
@@ -0,0 +1,4 @@
+package com.onlineShoppingBe.beForOnlineShoppingSystem.util;
+
+public class UserServiceImpl {
+}
